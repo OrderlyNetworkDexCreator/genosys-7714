4d283c1e0126dc6e13674fa4a7313ff461303bf8
Mario Reder <mario.reder@pm.me>
2026-02-19 17:43:50 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Genosys Exchange | The Next-Gen Perp DEX for All Traders",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Leaderboard,Markets,Swap,Vaults",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Leaderboard,Markets,Swap,Vaults,Points",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
